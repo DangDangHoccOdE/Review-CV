1c0e66dc5bcc2d1ab41f079a6fc8f5664e144864
DangDangHoccOdE <haidang972004@gmail.com>
2025-05-06 23:28:56 +0700

    config manager-router

--- client/src/app/manager-router/manager.module.ts
+++ client/src/app/manager-router/manager.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { managerRoutes } from './manager.routes';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(managerRoutes)
   ]
 })
 export class ManagerRouterModule { }
--- client/src/app/manager-router/manager.routes.ts
+++ client/src/app/manager-router/manager.routes.ts
@@ -1,8 +1,36 @@
+import { ProfileUserComponent } from './../components/profile-user/profile-user.component';
 import { Routes } from "@angular/router";
+import { ManagerLayoutComponent } from "../manager/manager-layout/manager-layout.component";
+import { JobLayoutComponent } from "../manager/job-layout/job-layout.component";
+import { EmployeeLayoutComponent } from "../manager/employee-layout/employee-layout.component";
+import { HrLayoutComponent } from "../manager/hr-layout/hr-layout.component";
+import { CompanyComponent } from "../manager/company/company.component";
 
 export const managerRoutes: Routes = [
     {
         path: '',
-        component: ManagerLa
+        component: ManagerLayoutComponent,
+        children:[
+            {
+                path: 'job',
+                component: JobLayoutComponent
+            },
+            {
+                path: 'employee',
+                component: EmployeeLayoutComponent
+            },
+            {
+                path: 'hr',
+                component: HrLayoutComponent
+            },
+            {
+                path: 'about',
+                component: CompanyComponent
+            },
+            {
+                path: 'profile/profile-user/:id',
+                component: ProfileUserComponent
+            }
+        ]
     }
 ]
