89c19490cb7af5e3d0c451aadaf1f1b5e73db2f2
DangDangHoccOdE <haidang972004@gmail.com>
2025-05-06 23:48:01 +0700

    fix css

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,70 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterLink, RouterOutlet } from '@angular/router';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { UserComponent } from './components/user/user.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ManagerLayoutComponent } from './manager/manager-layout/manager-layout.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, RouterLink,CommonModule, AdminLayoutComponent, NavbarComponent,UserComponent, LoginComponent, RegisterComponent, ManagerLayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'client';
+  title = 'angular';
+  showMainNavbar = true;
+  showAdminNavbar = false;
+  showUser = false;
+  showLogin = false;
+  showManagerNavbar = false;
+
+  constructor(
+    private router: Router
+  ){}
+
+  ngOnInit():void{
+    this.router.events.subscribe((event) => {
+      if(event instanceof NavigationEnd){
+        if(this.router.url.startsWith('/admin')){
+          this.showMainNavbar = false;
+          this.showAdminNavbar = true;
+          this.showUser = false;
+          this.showLogin = false;
+          this.showManagerNavbar = false;
+        } } else if (this.router.url.startsWith('/register') || this.router.url.startsWith('/user')) {
+          this.showMainNavbar = false;
+          this.showAdminNavbar = false;
+          this.showUser = true;
+          this.showLogin = false;
+          this.showManagerNavbar = false;
+        } else if(this.router.url.startsWith('/login')) {
+          this.showMainNavbar = false;
+          this.showAdminNavbar = false;
+          this.showUser = false;
+          this.showLogin = true;
+          this.showManagerNavbar = false;
+        } else if(this.router.url.startsWith('/manager')){
+          this.showMainNavbar = false;
+          this.showAdminNavbar = false;
+          this.showUser = false;
+          this.showLogin = false;
+          this.showManagerNavbar = true;
+        }
+        else {
+          this.showMainNavbar = true;
+          this.showAdminNavbar = false;
+          this.showUser = false;
+          this.showLogin = false;
+          this.showManagerNavbar = false;
+        }
+        
+      }
+    )
+  }
 }
+
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,74 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { CreateProfileComponent } from './components/create-profile/create-profile.component';
+import { ProfileListComponent } from './components/profile-list/profile-list.component';
+import { ProfileUserComponent } from './components/profile-user/profile-user.component';
+import { JobListComponent } from './components/job-list/job-list.component';
+import { JobDetailsComponent } from './components/job-details/job-details.component';
+import { ProjectListComponent } from './components/project-list/project-list.component';
+import { UserComponent } from './components/user/user.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'profile',
+        component: ProfileComponent,
+        children:[
+            {
+                path:'create',
+                component:CreateProfileComponent
+            },
+            {
+                path:'list-profiles',
+                component:ProfileListComponent
+            },
+            {
+                path: 'profile-user/:id?',
+                component: ProfileUserComponent
+            }
+        ]
+    },
+    {
+        path: 'job-list',
+        component: JobListComponent
+    },
+    {
+        path: 'job-details/:id',
+        component: JobDetailsComponent
+    },
+    {
+        path: 'list-project',
+        component: ProjectListComponent
+    },
+    {
+        path: 'user',
+        component: UserComponent
+    },
+    {
+        path: 'register',
+        component: RegisterComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'admin',
+        loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
+    },
+    {
+        path: 'manager',
+        loadChildren: () => import('./manager-router/manager.module').then(m => m.ManagerRouterModule)
+    }
+];
